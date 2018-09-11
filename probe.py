ebe82824dc883d50598c3071f79779f989909401
peter-peterson <peter@inlandempiregrowing.com>
2018-09-11 09:09:31 -0700

    modified probe file

--- probe.py
+++ probe.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python
-
 import io         # used to create file streams
 import fcntl      # used to access I2C parameters like addresses
 
 import time       # used for sleep delay and timestamps
 import string     # helps parse strings
-
+import datetime as dt
+import pandas as pd
 
 class AtlasI2C:
 	long_timeout = 1.5         	# the timeout needed to query readings and calibrations
@@ -13,8 +12,10 @@ class AtlasI2C:
 	default_bus = 1         	# the default bus for I2C on the newer Raspberry Pis, certain older boards use bus 0
 	default_address = 98     	# the default address for the sensor
 	current_addr = default_address
+	std_addr = {'DO': 97, 'ORP':98, 'PH':99, 'EC':100, 'RTD':102, 'PMP':103}
+	probe_data_store = 'probe_store.h5'
 
-	def __init__(self, address=default_address, bus=default_bus):
+	def __init__(self, addresses=[default_address], bus=default_bus, store_data=True):
 		# open two file streams, one for reading and one for writing
 		# the specific I2C channel is selected with bus
 		# it is usually 1, except for older revisions where its 0
@@ -22,8 +23,23 @@ class AtlasI2C:
 		self.file_read = io.open("/dev/i2c-"+str(bus), "rb", buffering=0)
 		self.file_write = io.open("/dev/i2c-"+str(bus), "wb", buffering=0)
 
-		# initializes I2C to either a user specified or default address
-		self.set_i2c_address(address)
+		
+		#create dict of connected probes and associated readings(all values start at 0.0
+		i2c_devices = self.list_i2c_devices()
+		probe_to_reading = {}
+		for probe, address in std_addr.iteritems():
+			if address in i2c_devices:
+				probe_to_reading[probe] = 0.0 # add probe to dict
+
+		# merge two dicts for eventual pd.dataframe
+		data_dict = {**{'datetime':[dt.datetime.now()]},
+					 **{key:[value] for (key, value) in probe_to_reading}}
+
+		self.probe_dataframe = pd.DataFrame(data_dict, columns = data_dict.keys())
+		self.probe_dataframe.index = self.probe_dataframe['datetime']
+		del df['datetime']
+
+
 
 	def set_i2c_address(self, addr):
 		# set the I2C communications to the slave specified by the address
@@ -67,6 +83,35 @@ class AtlasI2C:
 
 		return self.read()
 
+	def read_all_probes(query = "R"):
+		'''
+			Write to probes(query by defualt) and wait, then read from each address, 
+			and update reading(s) dict.  Is there a better/cleaner way to do this...?
+		'''
+		#start_time = dt.datetime.now()
+		for probe, address in std_addr.iteritems():
+			self.set_i2c_address(address)
+			self.write(query)
+		time.sleep(self.long_timeout)
+		for probe, address in std_addr.iteritems():
+			self.set_i2c_address(address)
+			self.probe_to_reading[probe] = self.read()
+		#end_time = dt.datetime.now()
+
+		self.store_data()
+
+	def store_data(self, max_samples = 60*60):
+
+		now = dt.datetime.now()
+		data = {key:[value] for (key, value) in self.probe_to_reading}
+		self.probe_dataframe = self.probe_dataframe.append(pd.DataFrame(data, index = [now]))
+
+		if len(self.probe_dataframe) >= max_samples:
+			# we write half data to disk and keep the rest in memory for potential data analysis
+			save_frame = self.probe_dataframe.tail(max_samples/2)
+			self.probe_dataframe = self.probe_dataframe.head(max_samples/2)
+			save_frame.to_hd5(probe_data_store)
+
 	def close(self):
 		self.file_read.close()
 		self.file_write.close()
@@ -84,64 +129,52 @@ class AtlasI2C:
 		self.set_i2c_address(prev_addr) # restore the address we were using
 		return i2c_devices
 
-
-		
-def main():
-	device = AtlasI2C() 	# creates the I2C port object, specify the address or bus if necessary
-
-	print(">> Atlas Scientific sample code")
-	print(">> Any commands entered are passed to the board via I2C except:")
-	print(">>   List_addr lists the available I2C addresses.")
-	print(">>   Address,xx changes the I2C address the Raspberry Pi communicates with.")
-	print(">>   Poll,xx.x command continuously polls the board every xx.x seconds")
-	print(" where xx.x is longer than the %0.2f second timeout." % AtlasI2C.long_timeout)
-	print(">> Pressing ctrl-c will stop the polling")
-	
-	# main loop
-	while True:
-		inputt = input("Enter command: ")
-
-		if inputt.upper().startswith("LIST_ADDR"):
-			devices = device.list_i2c_devices()
-			for i in range(len (devices)):
-				print (devices[i])
-
-		# address command lets you change which address the Raspberry Pi will poll
-		elif inputt.upper().startswith("ADDRESS"):
-			addr = int(str.split(inputt, ',')[1])
-			device.set_i2c_address(addr)
-			print("I2C address set to " + str(addr))
-
-		# continuous polling command automatically polls the board
-		elif inputt.upper().startswith("POLL"):
-			delaytime = float(str.split(inputt, ',')[1])
-
-			# check for polling time being too short, change it to the minimum timeout if too short
-			if delaytime < AtlasI2C.long_timeout:
-				print("Polling time is shorter than timeout, setting polling time to %0.2f" % AtlasI2C.long_timeout)
-				delaytime = AtlasI2C.long_timeout
-
-			# get the information of the board you're polling
+	def print_i2c_devices(self):
+		device_list = self.list_i2c_devices()
+		for device_addr in device_list:
+			self.set_i2c_address(device_addr)
 			info = str.split(device.query("I"), ",")[1]
-			print("Polling %s sensor every %0.2f seconds, press ctrl-c to stop polling" % (info, delaytime))
+			print("{0} : {1}".format(device_addr,info))
 
-			try:
-				while True:
-					print(device.query("R"))
-					time.sleep(delaytime - AtlasI2C.long_timeout)
-			except KeyboardInterrupt: 		# catches the ctrl-c command, which breaks the loop above
-				print("Continuous polling stopped")
+def main():
 
-		# if not a special keyword, pass commands straight to board
+device = AtlasI2C()
+device.print_i2c_devices()
+
+while True:
+
+	inputt = input("Enter command: ")
+
+	if inputt.upper().startswith("LIST_ADDR"):
+		device.print_i2c_devices()
+
+	# continuous polling command automatically polls the board
+	elif inputt.upper().startswith("RUN"):
+
+		try:
+			while True:
+				device.read_all_probes()
+				print(device.probe_to_reading)
+		except KeyboardInterrupt: 		# catches the ctrl-c command, which breaks the loop above
+			print("Continuous polling stopped")
+
+	# if not a special keyword, pass commands straight to board
+	else:
+		if len(inputt) == 0:
+			print ("Please input valid command.")
 		else:
-			if len(inputt) == 0:
-				print ("Please input valid command.")
-			else:
-				try:
-					print(device.query(inputt))
-				except IOError:
-					print("Query failed \n - Address may be invalid, use List_addr command to see available addresses")
+			try:
+				print(device.query(inputt))
+			except IOError:
+				print("Query failed \n - Address may be invalid, use List_addr command to see available addresses")
 
 
-if __name__ == '__main__':
-	main()
+
+ 
+#Stanard Probe Addresses
+#EZO DO: 97 (0x61)
+#EZO ORP: 98 (0x62)
+#EZO pH: 99 (0x63)
+#EZO EC: 100 (0x64)
+#EZO RTD: 102 (0x66)
+#EZO PMP: 103 (0x67)
